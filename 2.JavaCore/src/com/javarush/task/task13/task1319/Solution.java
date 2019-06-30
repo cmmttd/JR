70d4655d70d83357d86e1b50186397abac21c0bd
cmmttd <forledwatch@gmail.com>
2019-06-30 15:20:50 +0400

    add some tasks 13 lvl

--- 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -1,9 +1,8 @@
 package com.javarush.task.task13.task1319;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /* 
 Писатель в файл с консоли
@@ -12,5 +11,31 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) {
         // напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> strings = new ArrayList<>();
+
+        String fileName;
+        String str;
+        try {
+
+            fileName = reader.readLine();
+            while (true) {
+                str = reader.readLine();
+                strings.add(str);
+                if (str.equals("exit")) break;
+            }
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(new File(fileName)));
+
+            for (String s : strings) {
+                bw.write(s + '\n');
+            }
+
+            bw.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 }
