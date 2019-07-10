cb78484e396009705eb4a6bd3bda0fd9f6c6b176
cmmttd <forledwatch@gmail.com>
2019-07-10 23:33:38 +0400

    Euclid

--- 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -4,7 +4,30 @@ package com.javarush.task.task14.task1420;
 НОД
 */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int fst, sec;
+        fst = Integer.parseInt(reader.readLine());
+        if (fst <= 0) throw new Exception();
+        sec = Integer.parseInt(reader.readLine());
+        if (sec <= 0) throw new Exception();
+
+        int min = fst < sec ? fst : sec;
+        int max = fst > sec ? fst : sec;
+        int eu = max % min;
+
+        while (eu != 0){
+            max = min;
+            min = eu;
+            eu = max % min;
+        }
+        System.out.println(min);
+
+        reader.close();
     }
 }
