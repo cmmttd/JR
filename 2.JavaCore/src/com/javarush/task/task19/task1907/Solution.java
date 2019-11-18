00f13e31385ed5f8259691e63482a51e95aac2d9
cmmttd <forledwatch@gmail.com>
2019-11-18 23:18:47 +0400

    autopush

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1907/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task19.task1907;
+
+/* 
+Считаем слово
+*/
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        FileReader fr = new FileReader(br.readLine());
+        br.close();
+        StringBuilder sb = new StringBuilder();
+        while (fr.ready()){
+            sb.append((char)fr.read());
+        }
+        fr.close();
+//        #1  validator say something wrong...
+        int len = sb.toString().split("world", -1).length - 1;
+        System.out.println(len < 0 ? 0 : len);
+//        sb.toString().stream
+    }
+}
