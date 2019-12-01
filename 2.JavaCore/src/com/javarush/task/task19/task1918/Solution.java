4809e17067e890a4ad5d62b3cc0860762805f524
cmmttd <forledwatch@gmail.com>
2019-12-01 16:59:01 +0400

    autopush

--- 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1918/Solution.java
@@ -7,27 +7,20 @@ package com.javarush.task.task19.task1918;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import org.jsoup.Jsoup;
+import org.jsoup.parser.Parser;
 
 public class Solution {
     public static void main(String[] args) throws Exception{
-//        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedReader file = new BufferedReader(new FileReader(console.readLine()));
-//        console.close();
-        BufferedReader file = new BufferedReader(new FileReader("D:\\Prog\\JR\\JavaRushTasks\\2.JavaCore\\src\\com\\javarush\\task\\task19\\task1918\\test.html"));
+        BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader file = new BufferedReader(new FileReader(console.readLine()));
+        console.close();
 
         String cont = file.lines().collect(Collectors.joining(""));
         file.close();
 
-        System.out.println(cont);
-
-        Matcher st = Pattern.compile("<" + args[0]).matcher(cont);
-        Matcher fin = Pattern.compile("/" + args[0] + ">").matcher(cont);
-
-        System.out.println(st.results().count());
-        System.out.println(fin.results().count());
+        System.out.println(Jsoup.parse(cont, "", Parser.xmlParser()).select(args[0]));
     }
 }
 
