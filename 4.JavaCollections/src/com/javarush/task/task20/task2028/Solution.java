1e89dac87315c765d470f829b343b60d59542b89
cmmttd Home <forledwatch@gmail.com>
2020-02-01 00:08:32 +0300

    save local changes

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/Solution.java
@@ -4,6 +4,6 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        List<String> list = new CustomTree();
+       // List<String> list = new CustomTree();
     }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3201/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task32.task3201;
+
+import java.io.RandomAccessFile;
+
+/*
+Запись в существующий файл
+*/
+public class Solution {
+    public static void main(String... args) {
+//        String file = args[0];
+//        int position = Integer.parseInt(args[1]);
+//        String data = args[2];
+
+        System.out.println("123");
+
+//        try (RandomAccessFile raf = new RandomAccessFile(file, "rw")) {
+//            raf.seek(position);
+//            raf.writeUTF(data);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+}
