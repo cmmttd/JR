899b08b22137ae125562573510b7d0f5930700d0
VBelogrudov <forledwatch@gmail.com>
2020-03-04 10:13:47 +0300

    xml unmarshalling task

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3308/Shop.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task33.task3308;
+
+import javax.xml.bind.annotation.*;
+import java.util.Arrays;
+import java.util.List;
+
+@XmlRootElement(name = "shop")
+public class Shop {
+    public Goods goods;
+    public int count;
+    public double profit;
+    public String[] secretData;
+
+    @XmlRootElement(name = "goods")
+    public static class Goods {
+
+        public List<String> names;
+
+    public Goods() {
+    }
+}
+
+    @Override
+    public String toString() {
+        return "Shop{" +
+                "goods=" + goods.names +
+                ", count=" + count +
+                ", profit=" + profit +
+                ", secretData=" + Arrays.toString(secretData) +
+                '}';
+    }
+
+    public Shop() {
+    }
+}
